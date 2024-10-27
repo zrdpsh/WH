1b347635b9b996cd824b149b01e6d666c162ce09
zrdpsh <zerdposh@gmail.com>
2024-10-27 20:43:07 +0300

    Time of Life added

---
+++ TimeOfLifeRefactored/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        // code
+    }
+}
