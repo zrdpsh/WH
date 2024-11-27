39970c30f7b6e8d2aec98d5ca9130be28643ae9f
zrdpsh <zerdposh@gmail.com>
2024-11-27 17:02:11 +0300

    all single numerals converted as expected

--- TimeOfLifeRefactored/src/main/java/org/example/RomanToArabicTests.java
+++ TimeOfLifeRefactored/src/main/java/org/example/RomanToArabicTests.java
@@ -9,8 +9,19 @@ public class RomanToArabicTests {
     private final RomanToArabic converter = new RomanToArabic();
 
     @Test
-    void testingSingleNumerals() {
+    void testingSingleNumeral() {
         assertEquals(1, converter.convertRomanToArabic("I"));
+    }
+
+    @Test
+    void testingAllSingleNumerals() {
+        assertEquals(1, converter.convertRomanToArabic("I"));
+        assertEquals(5, converter.convertRomanToArabic("V"));
+        assertEquals(10, converter.convertRomanToArabic("X"));
+        assertEquals(50, converter.convertRomanToArabic("L"));
+        assertEquals(100, converter.convertRomanToArabic("C"));
+        assertEquals(500, converter.convertRomanToArabic("D"));
+        assertEquals(1000, converter.convertRomanToArabic("M"));
     }
 
 }
