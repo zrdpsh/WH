6cd6ce1964a61c95e51f8ecdfe4d6ee3c42d5425
zrdpsh <zerdposh@gmail.com>
2024-12-09 19:23:55 +0300

    SavingAccount class updated

--- TimeOfLifeRefactored/src/main/java/org/example/Account/SavingAccount.java
+++ TimeOfLifeRefactored/src/main/java/org/example/Account/SavingAccount.java
@@ -1,4 +1,12 @@
 package org.example.Account;
 
-public class SavingAccount {
+public class SavingAccount extends Account{
+    public SavingAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public double calculateTax() {
+        return balance * 0.10;
+    }
 }
