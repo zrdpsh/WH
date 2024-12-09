5782d638847f5604a11476ea55d756990273fa35
zrdpsh <zerdposh@gmail.com>
2024-12-09 19:31:16 +0300

    MutualDepositAccount class updated

--- TimeOfLifeRefactored/src/main/java/org/example/Account/MutualFundAccount.java
+++ TimeOfLifeRefactored/src/main/java/org/example/Account/MutualFundAccount.java
@@ -1,4 +1,12 @@
 package org.example.Account;
 
 public class MutualFundAccount extends Account{
+    public MutualFundAccount(double balance) {
+        super(balance);
+    }
+
+    @Override
+    public double calculateTax() {
+        return balance * 0.45;
+    }
 }
